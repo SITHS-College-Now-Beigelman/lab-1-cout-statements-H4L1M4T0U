31771d5edff097e14f0fd8b82048cabbe8729c6d
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-09-30 16:07:21 -0400

    sdfsdfsf
    werwefdfds

---
+++ Revised_Lab.cpp
@@ -0,0 +1,39 @@
+//lab 1//
+// Re-do (I joined College Now Later)
+//September 13, 2024//
+#include <iostream>
+//#include <iostream> allows us to basically cout and generate the output towards the end of our perimiter proj.
+using namespace std;
+// namespace std basically allows cout and endl to be used w/o the prefix std::
+int main() {
+//The function 'main' is basically needed for every c ++ program
+cout << "###########################################################################" << endl;
+cout << "#                                                                         # "<< endl;
+cout << "# Ways to access the Task Manager on your Windows computer:               #" << endl;
+cout << "#                                                                         #" << endl;
+cout << "# Press the key combination Ctrl + Shift + Escape                         #" << endl;
+cout << "#                                                                         #"<< endl;
+cout << "#Press  the key Combination Crtl + + Alt + Delete and Select Task Manager #" << endl;
+cout << "#                                                                         #" << endl;
+cout << "#Type Task manager in the Windows Start menu search                       #" << endl;
+cout << "#                                                                         #" << endl;
+cout <<"#                                                                          #" << endl;
+cout << "###########################################################################" << endl;
+// i used the cout statements to make a box (i had to add a few extra hashtags bc one of the lines were too long
+return 0;
+// the '//' funcion comments the bottom text below out vvv
+
+//###########################################################################
+//#                                                                         # 
+//#Ways to access the Task Manager on your Windows computer:                #
+//#                                                                         #
+//# Press the key combination Ctrl + Shift + Escape                         #
+//#                                                                         #
+//#Press  the key Combination Crtl + + Alt + Delete and Select Task Manager #
+//#                                                                         #
+//#Type Task manager in the Windows Start menu search                       #
+//#                                                                         #
+//#                                                                         #
+//###########################################################################
+
+}
